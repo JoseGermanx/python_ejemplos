fce627a0a88a4abee83899a3c16573bd098dec85
Jose G Martinez <jgermanmm@gmail.com>
2026-01-20 17:27:35 -0300

    optimizacion

---
+++ diccionarios_optimizacion.py
@@ -0,0 +1,13 @@
+cuadrados = []
+for x in range(10):
+    cuadrados.append(x**2)
+
+print(cuadrados)
+
+cuadrados = [x**2 for x in range(10)]
+
+print(cuadrados)
+
+pares = {x: x**2 for x in range(5)}
+
+print(cuadrados)
